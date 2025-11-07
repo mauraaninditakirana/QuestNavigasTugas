2fe210a4fd7c25c8890a14c8d954747de7ea72fd
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-07 10:06:50 +0700

    Membuat file Navigasi.kt untuk menangani navigasi aplikasi.

--- app/src/main/java/com/example/questnavigastugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas/Navigasi.kt
@@ -1,2 +1,35 @@
 package com.example.questnavigastugas
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas.view.Formulir
+import com.example.questnavigastugas.view.HalamanAwal
+import com.example.questnavigastugas.view.ListDaftar
+
+object Routes {
+    const val HALAMAN_AWAL = "HalamanAwal"
+    const val LIST_DAFTAR = "ListDaftar"
+    const val FORMULIR = "Formulir"
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HALAMAN_AWAL
+    ) {
+        composable(Routes.HALAMAN_AWAL) {
+            HalamanAwal(navController = navController)
+        }
+        composable(Routes.LIST_DAFTAR) {
+            ListDaftar(navController = navController)
+        }
+        composable(Routes.FORMULIR) {
+            Formulir(navController = navController)
+        }
+    }
+}
